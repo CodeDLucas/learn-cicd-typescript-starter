22b00a3198f9cb0c91a7506a90bd4ffa27b8dca5
LcsLD <Lcslirio@gmail.com>
2025-06-05 11:28:35 -0300

    'Fail was a success' now do the right test

--- src/tests/sum.test.js
+++ src/tests/sum.test.js
@@ -1,7 +1,6 @@
 import { expect, test } from 'vitest'
 import { sum } from './sum.js'
 
-// changed file to fail this time
 test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(4)
+  expect(sum(1, 2)).toBe(3)
 })
